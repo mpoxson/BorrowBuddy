dfef85a33a106ed7da1b3c5001eef51a7c803465
xiachen97 <xiachen2@oakland.edu>
2024-02-01 10:32:09 -0500

    update database relationship

--- server/models/products.js
+++ server/models/products.js
@@ -1,50 +1,61 @@
-module.exports=(sequelize,DataTypes)=>{
-
-    const products=sequelize.define("products",{
+module.exports = (sequelize, DataTypes) => {
+    
+    const products = sequelize.define("products", {
         product_id: {
             type: DataTypes.INTEGER,
-            primaryKey: true,
+            primaryKey: true,//Set as primary key
             autoIncrement: true,
         },
-        product_name:{
-            type:DataTypes.STRING,
-            allowNull:false,
+        product_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
         },
-        product_category:{
-            type:DataTypes.STRING,
-            allowNull:false,
+        product_category: {
+            type: DataTypes.STRING,
+            allowNull: false,
         },
-        product_description:{
-            type:DataTypes.STRING,
-            allowNull:false,
+        product_description: {
+            type: DataTypes.TEXT,
+            allowNull: false,
         },
-        product_price:{
-            type:DataTypes.STRING,
-            allowNull:false,
+        product_price: {
+            type: DataTypes.STRING,
+            allowNull: false,
         },
-        product_available_time:{
-            type:DataTypes.STRING,
-            allowNull:false,
+        product_available_start_time: {
+            type: DataTypes.DATE,
+            allowNull: false,
         },
-        product_image:{
-            type:DataTypes.STRING,
-            allowNull:false,
+        product_available_end_time: {
+            type: DataTypes.DATE,
+            allowNull: false,
         },
-        product_is_rented:{
-            type:DataTypes.STRING,
-            allowNull:false,
+        product_image: {
+            type: DataTypes.STRING,
+            allowNull: false,
         },
-        rentee_username:{
-            type:DataTypes.STRING,
-            allowNull:false,
+        owner_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: { //Set as foreign key
+                model: 'users', // associated table name
+                key: 'user_id' // Associated field name
+            }
         },
- 
+        product_is_rented: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        rentee_username: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
     },
     {
         timestamps: false,
         createdAt: false,
         updatedAt: false,
-    }
-    );
+    });
+
     return products;
-}
+};
