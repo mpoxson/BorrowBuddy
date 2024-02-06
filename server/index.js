1dceab539f6b14a78a4faada028e5bfe631acee5
Mira Gutzman <64936023+Goofzillers@users.noreply.github.com>
2024-02-05 22:56:27 -0500

    added new tables

--- server/index.js
+++ server/index.js
@@ -12,6 +12,15 @@ app.use("/products",productsRouter);
 const usersRouter=require('./routes/users')
 app.use("/users",usersRouter);
 
+const commentsRouter=require('./routes/comments')
+app.use("/comments",commentsRouter);
+
+const ratingsRouter=require('./routes/ratings')
+app.use("/ratings",ratingsRouter);
+
+const product_rentalsRouter=require('./routes/product_rentals')
+app.use("/product_rentals",product_rentalsRouter);
+
 db.sequelize.sync().then(()=>{
     app.listen(3001, ()=>{
         console.log('Server running on port 3001')
