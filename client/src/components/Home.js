5bd3fe22be5f24de670055c4e63b0254d3939ea2
xiachen97 <xiachen2@oakland.edu>
2024-02-08 16:46:41 -0500

    Home Page Updated

---
+++ client/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Welcome to Home Page...we will create nav bar page and more page link/page here...</h2>
+      {/* add more content here... */}
+    </div>
+  );
+};
+
+export default Home;
