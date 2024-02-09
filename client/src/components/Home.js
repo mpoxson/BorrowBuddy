e4cdd8d38ca0687328e0e2ee30637d72c41e1996
xiachen97 <xiachen2@oakland.edu>
2024-02-08 20:24:09 -0500

    change web tab name

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h2>Welcome to Home Page...we will create nav bar page and more page link/page here...</h2>
+      <h2>Welcome to Home Page...</h2>
+      <h2>we will create nav bar page and more page/link here...</h2>
       {/* add more content here... */}
     </div>
   );
