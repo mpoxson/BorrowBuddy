4b7f43c23e33a2b3674320022335a5d0c9c92e5f
mpoxson <mpoxson@gmail.com>
2024-02-13 10:33:36 -0500

    Initial navbar added

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,25 @@ import Login from "./components/Login";
 import Register from "./components/Register";
 import Home from "./components/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Button from "@mui/material/Button";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <Router>
           <Routes>
             <Route path="/UsersList" element={<TestUsersList />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
 
             {/* define some other routes here...*/}
           </Routes>
         </Router>
       </header>
-      <Button variant="contained">Hello World</Button>
     </div>
   );
 }
