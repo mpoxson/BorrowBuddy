e7e8af2ebd166f76a6f64debcd44418edfff8473
mpoxson <mpoxson@gmail.com>
2024-02-13 23:01:41 -0500

    Updated navbar, added enumerated fields

--- client/src/App.js
+++ client/src/App.js
@@ -5,24 +5,43 @@ import Register from "./components/Register";
 import Home from "./components/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { COLORS as c } from "./constants/enums";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: c.PRIMARY,
+    },
+    secondary: {
+      main: c.SECONDARY,
+    },
+    accent: {
+      main: c.ACCENT,
+    },
+  },
+});
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <header className="App-header">
-        <Router>
-          <Routes>
-            <Route path="/UsersList" element={<TestUsersList />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
+      <ThemeProvider theme={theme}>
+        <Navbar />
 
-            {/* define some other routes here...*/}
-          </Routes>
-        </Router>
-      </header>
+        <header className="App-header">
+          <Router>
+            <Routes>
+              <Route path="/UsersList" element={<TestUsersList />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+
+              {/* define some other routes here...*/}
+            </Routes>
+          </Router>
+        </header>
+      </ThemeProvider>
     </div>
   );
 }
---
+++ client/src/constants/enums.js
@@ -0,0 +1,14 @@
+export const PAGE_ROUTES = {
+  HOME: "/",
+  REGISTER: "/register",
+  USERS: "/login",
+  LOGIN: "/UsersList",
+};
+
+export const PAGE_NAMES = ["Home", "Register", "Login", "UsersList"];
+
+export const COLORS = {
+  PRIMARY: "#001d3d",
+  SECONDARY: "#ebebeb",
+  ACCENT: "#ffc300",
+};
