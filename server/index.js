70da11d19b9fe453f1f36cb971b89919b3a3b052
xiachen97 <xiachen2@oakland.edu>
2024-02-25 23:27:13 -0500

    Test Router ProductImage Success

--- server/index.js
+++ server/index.js
@@ -25,6 +25,9 @@ app.use("/product_rentals", product_rentalsRouter);
 const product_savesRouter = require("./routes/product_saves");
 app.use("/product_saves", product_savesRouter);
 
+const product_imagesRouter = require("./routes/product_images");
+app.use("/product_images", product_imagesRouter);
+
 db.sequelize.sync().then(() => {
   app.listen(3001, () => {
     console.log("Server running on port 3001");
