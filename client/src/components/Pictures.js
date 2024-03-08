f4b191b36294a375b84d1c9b6d654f0bde1aa5db
mpoxson <123969470+mpoxson@users.noreply.github.com>
2024-03-08 15:04:03 -0500

    Carousel for images

---
+++ client/src/components/Pictures.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+
+const Pictures = ({ props }) => {
+  return (
+    <Swiper
+      navigation={true}
+      modules={[Navigation]}
+      className="mySwiper"
+      slidesPerView={2}
+      spaceBetween={30}
+      centeredSlides={true}
+    >
+      {props.map((image) => (
+        <SwiperSlide>
+          <img src={image} style={{ width: "100%" }} />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default Pictures;
--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -5,28 +5,33 @@ import { COLORS } from "../constants/enums";
 import Avatar from "@mui/material/Avatar";
 import Grid from "@mui/material/Unstable_Grid2";
 import Paper from "@mui/material/Paper";
-import Textarea from "@mui/joy/Textarea";
+import { TextareaAutosize } from "@mui/base/TextareaAutosize";
+import Pictures from "./Pictures";
 
-const Product = (props) => {
+import img from "../image/test.jpg";
+
+const Product = () => {
   let { productId } = useParams();
   console.log(`${productId}`);
+
+  const img_array = [img, img, img];
+
   return (
-    <Container maxWidth="xl">
+    <Container
+      maxWidth="xxl"
+      sx={{
+        paddingTop: "10px",
+        display: "block",
+        justifyContent: "center",
+      }}
+    >
       {/* Box for product info */}
       <Box>
-        {/* <ImageList sx={{ width: 500, height: 450 }} cols={1} rowHeight={164}>
-          {itemData.map((item) => (
-            <ImageListItem key={item.img}>
-              <img
-                srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
-                alt={item.title}
-                loading="lazy"
-              />
-            </ImageListItem>
-          ))}
-        </ImageList> */}
-
+        <Box display={"flex"} justifyContent={"center"} maxWidth={"100%"}>
+          <div style={{ width: "100%" }}>
+            <Pictures props={img_array} />
+          </div>
+        </Box>
         {/* Box for user info section */}
         <Container>
           <Box>
@@ -98,7 +103,7 @@ const Product = (props) => {
           </Paper>
         </Box>
         <Box>
-          <Textarea />
+          <TextareaAutosize />
           <Button>Submit</Button>
         </Box>
       </Box>
