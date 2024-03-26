f7d71c07825507e207b814aa269669e62beaf64c
xiachen97 <xiachen2@oakland.edu>
2024-03-26 00:40:34 -0400

    Add delete and put API for product

--- server/routes/products.js
+++ server/routes/products.js
@@ -31,5 +31,44 @@ router.post("/", async (req,res)=>{
      
 })//This is for frontend 
 
+router.put("/:productId", async (req, res) => {
+    const productId = req.params.productId;
+  
+    try {
+      const product = await product.findByPk(productId);
+      if (!product) {
+        return res.status(404).json({ message: "Poduct not found" });
+      }
+  
+      // Update product fields based on the request body
+      await product.update(req.body);
+  
+      res.json({ message: "Product updated successfully" });
+    } catch (error) {
+      console.error("Error updating product:", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  });
+  
 
+router.delete("/:productId", async (req, res) => {
+    const productId = req.params.productId;
+  
+    try {
+      const product = await products.findByPk(productId);
+      if (!product) {
+        return res.status(404).json({ message: "Product not found" });
+      }
+  
+      // Delete the product
+      await product.destroy();
+  
+      res.json({ message: "Product deleted successfully" });
+    } catch (error) {
+      console.error("Error deleting product:", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  });
+
+  
 module.exports=router;
