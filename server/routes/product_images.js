39a6aa9a803e2a4baeb31aa7ce1bd7f6e5c147ea
Mira Gutzman <swss_goofzilla@live.com>
2024-03-30 15:58:40 -0400

    Card images / product page images
    Display product images and pfps from DB instead of calling Firebase, new product_images routes

--- server/routes/product_images.js
+++ server/routes/product_images.js
@@ -8,6 +8,47 @@ router.get('/',async (req,res)=>{
     res.json(listOfProductimages);
 });
 
+//Associated with product ID... Image URL only
+router.get('/:productId', async (req, res) => {
+    const product_id = req.params.productId;
+
+    try {
+        const imagesByProduct = await product_images.findAll({
+            where: {
+                product_id: product_id
+            },
+            order: [
+                ['image_order', 'ASC'],
+            ],
+            attributes: ['image_location']
+        });
+        res.json(imagesByProduct);
+    } catch (error) {
+        console.error('Error fetching product images:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+//Associated with product ID, where image_order value is lowest...
+router.get('/min/:productId', async (req, res) => {
+    const product_id = req.params.productId;
+
+    try {
+        const imagesByProduct = await product_images.findOne({
+            where: {
+                product_id: product_id
+            },
+            order: [
+                ['image_order', 'ASC'],
+            ]
+        });
+        res.json(imagesByProduct);
+    } catch (error) {
+        console.error('Error fetching product images:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
 router.post("/", async (req,res)=>{
     const product_image=req.body;
     await product_images.create(product_image);
